a5d4b680331f63fd08551e37a00ed9f22b5c7a0d
amenzai <amenzai@qq.com>
2018-09-29 15:32:29 +0800

    fix webpack config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  productHost: 'http://api.framework.vastsum.com',
+  // productHost: 'http://api.framework.vastsum.com',
+  productHost: 'http://demo.renren.io/renren-fast',
   proxyHost: 'http://demo.renren.io/renren-fast',
   cdnUrl: ''
 }
